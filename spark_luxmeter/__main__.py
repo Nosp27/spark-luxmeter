7f5ef165a71c2358831aab7f8792c462ec11a235
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2021-03-07 10:02:31 +0300

    Add test script

---
+++ spark_luxmeter/__main__.py
@@ -0,0 +1,10 @@
+from spark_luxmeter.spark_logs.loaders import abc
+
+
+if __name__ == "__main__":
+    l = abc.BaseMetricsLoader()
+    ids = [a["ID"] for a in l.load_application_info_list()]
+    our_id = ids[0]
+    print("id:", our_id)
+    data = l.load(our_id)
+    print(data)
